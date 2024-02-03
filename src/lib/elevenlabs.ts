b9a1a9403eed32c95586c7ac98b13cfd5ad6110f
Tristan Rechenberger <rechenberger@me.com>
2024-02-03 13:13:18 +0100

    🎉

--- src/lib/elevenlabs.ts
+++ src/lib/elevenlabs.ts
@@ -1,3 +1,8 @@
+import pLimit from 'p-limit'
+
+const ELEVENLABS_MAX_CONCURRENCY = 3
+const limit = pLimit(ELEVENLABS_MAX_CONCURRENCY)
+
 export const textToSpeechElevenLabs = async ({
   text,
   voiceId,
@@ -5,33 +10,35 @@ export const textToSpeechElevenLabs = async ({
   text: string
   voiceId: string
 }) => {
-  const tts_url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`
-  const result = await fetch(tts_url, {
-    method: 'POST',
-    headers: {
-      // accept: 'audio/mpeg',
-      'xi-api-key': process.env.ELEVENLABS_API_KEY!,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      text,
-      model_id: 'eleven_multilingual_v2',
-      voice_settings: {
-        stability: 0.5,
-        similarity_boost: 0.75,
+  return limit(async () => {
+    const tts_url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`
+    const result = await fetch(tts_url, {
+      method: 'POST',
+      headers: {
+        // accept: 'audio/mpeg',
+        'xi-api-key': process.env.ELEVENLABS_API_KEY!,
+        'Content-Type': 'application/json',
       },
-    }),
+      body: JSON.stringify({
+        text,
+        model_id: 'eleven_multilingual_v2',
+        voice_settings: {
+          stability: 0.4,
+          similarity_boost: 0.75,
+        },
+      }),
+    })
+
+    if (result.ok === false) {
+      throw new Error(await result.text())
+    }
+
+    // return await result.json()
+
+    const blob = await result.blob()
+
+    return {
+      blob,
+    }
   })
-
-  if (result.ok === false) {
-    throw new Error(await result.text())
-  }
-
-  // return await result.json()
-
-  const blob = await result.blob()
-
-  return {
-    blob,
-  }
 }
